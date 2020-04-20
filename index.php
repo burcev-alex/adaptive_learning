b6db11e4f8966ee047d6fd8cc386487fcab0db4a
Burtsev Alexander <Alexander.burzev@inbox.ru>
2020-04-20 15:12:42 +0300

    feat(moodle): Отправка уведомлений через RESTful

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+$urlServer = 'http://91.200.42.232/webservice/rest/server.php';
+$token = '8be9314e213c01a9a42b46c6e90d5352';
+
+$MoodleRest = new MoodleRest($urlServer, $token);
+
+$dataRequest = [
+    "notes" => [
+        [
+            "userid" => 1, //id of the user the note is about
+            "publishstate" => "personal",  //'personal', 'course' or 'site'
+            "text" => "Тестовое сообщение из <b>WS</b>",
+            "format" => 1
+        ]
+    ]
+];
+
+$parametersRequest = [
+    "notes" => [
+        [
+            "userid" => 1, //id of the user the note is about
+            "publishstate" => "personal",  //'personal', 'course' or 'site'
+            "courseid" => 2,
+            "text" => "Тестовое сообщение из <b>WS</b>",
+            "format" => 1
+        ]
+    ]
+];
+
+$note = $MoodleRest->request('core_notes_create_notes', $parametersRequest, MoodleRest::METHOD_POST);
+
+print_r($note);
