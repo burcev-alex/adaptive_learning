d98920dd6ae8a82221295272c1f3ff2e6ac154e7
Burtsev Alexander <Alexander.burzev@inbox.ru>
2020-04-21 13:56:18 +0300

    feat(api): Тестирование АПИ Moodle

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 $urlServer = 'http://91.200.42.232/webservice/rest/server.php';
-$token = '8be9314e213c01a9a42b46c6e90d5352';
+$token = '1818c0cedf5f484f9c767b6b135a12c1';
+
+$userId = 4; // Александр Бурцев
 
 $MoodleRest = new MoodleRest($urlServer, $token);
 
+function p($arr, $console = false){
+    if (!$console) {
+        echo '<pre>'.print_r($arr, true).'</pre>';
+    }
+    else{
+        print_r($arr);
+    }
+}
+
+/*
 $parametersRequest = [
     "notes" => [
         [
@@ -18,6 +30,30 @@ $parametersRequest = [
     ]
 ];
 
+// отправить персональное уведомление
 $note = $MoodleRest->request('core_notes_create_notes', $parametersRequest, MoodleRest::METHOD_POST);
 
-print_r($note);
+p($note);
+
+
+$parametersRequest = [
+    "courseids" => [
+        2
+    ]
+];
+
+// все тесты определенных курстов
+$data = $MoodleRest->request('mod_quiz_get_quizzes_by_courses', $parametersRequest, MoodleRest::METHOD_POST);
+
+p($data);
+*/
+
+$parametersRequest = [
+    "quizid" => 1,
+    "userid" => $userId
+];
+
+// Вернуть список попыток для данного теста и пользователя.
+$data = $MoodleRest->request('mod_quiz_get_user_attempts', $parametersRequest, MoodleRest::METHOD_POST);
+
+p($data);
